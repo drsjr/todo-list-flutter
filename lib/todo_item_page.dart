29d2a634b9e70a88984e5d1f37cb8845a7c8a7da
Junior <djalma.rsjr@gmail.com>
2022-01-06 23:49:44 -0300

    state management with ValueNotifier

--- lib/todo_item_page.dart
+++ lib/todo_item_page.dart
@@ -1,24 +1,56 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import 'widgets/custom_button_widget.dart';
 
-class TodoItemPage extends StatelessWidget {
+class TodoItemPage extends StatefulWidget {
   // final String itemText;
   // TodoItemPage(this.itemText);
 
   @override
+  _TodoItemPageState createState() => _TodoItemPageState();
+}
+
+class _TodoItemPageState extends State<TodoItemPage> {
+
+  ValueNotifier<int> valor = ValueNotifier<int>(0);
+
+  random() async {
+    for (int i = 0; i<10; i++) {
+      await Future.delayed(Duration(seconds: 1));
+      valor.value = Random().nextInt(1000);   
+    }
+  }
+
+
+  @override
   Widget build(BuildContext context) {
     var args = ModalRoute.of(context).settings.arguments as String;
+    print("build");
     return Scaffold(
       appBar: AppBar(),
       body: Center(
-        child: CustomButtonWidget(
-          () {
-
-          }, 
-          args, 
-          false)
-      ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ValueListenableBuilder(
+              valueListenable: valor, 
+              builder: (_, valor1, __) => Text(
+                'O valor aleatorio eh: $valor1', 
+                style: TextStyle(fontSize: 20.0))
+            ),
+            SizedBox(height: 16),
+            CustomButtonWidget(
+              () {
+                random();
+                //Navigator.of(context).pop();
+              }, 
+              args, 
+              false)
+          ],
+        ),
+      )
     );
   }
 }
